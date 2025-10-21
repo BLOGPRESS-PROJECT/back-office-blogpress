302d0d60150aa80b995c64952641b3623483f9e3
Ben Djibril <konedjibril2004@gmail.com>
2025-10-21 15:59:25 +0100

    Add security Configuration for this projetct

--- src/main/kotlin/com/kobe/blogpress_api/model/user/RoleType.kt
+++ src/main/kotlin/com/kobe/blogpress_api/model/user/RoleType.kt
@@ -24,6 +24,28 @@ enum class RoleType(
     }
 }
 
+object RolePermissionConfig {
+    private val rolePermissions = mapOf(
+        RoleType.ADMIN_SYSTEM to setOf(
+            Permission.GET_ALL_USERS, Permission.FULL_USER_CONTROL,
+            Permission.CREATE_USER, Permission.READ_USER, Permission.UPDATE_USER, Permission.DELETE_USER, Permission.ACTIVATE_USER, Permission.DEACTIVATE_USER,
+
+            Permission.CREATE_REQUEST, Permission.READ_OWN_REQUESTS, Permission.UPDATE_REQUEST, Permission.DELETE_REQUEST,
+            Permission.CANCEL_REQUEST, Permission.REVIEW_REQUEST_APPROVE_REQUEST, Permission.READ_ALL_REQUEST,
+
+            Permission.ASSIGN_ROLE, Permission.MODIFY_ROLE, Permission.RESET_PASSWORD
+        )
+    )
+
+    fun getPermissions(roleType: RoleType): Set<Permission> {
+        return rolePermissions[roleType] ?: emptySet()
+    }
+
+    fun hasPermission(roleType: RoleType, permission: Permission): Boolean {
+        return getPermissions(roleType).contains(permission)
+    }
+}
+
 
 enum class DeactivationReason(val description: String) {
     ADMIN_DECISION("Désactivé par un administrateur"),
