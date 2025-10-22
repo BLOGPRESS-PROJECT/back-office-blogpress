e5173543084739860d377e2c34e7ec09e08d358e
Ben Djibril <konedjibril2004@gmail.com>
2025-10-23 00:18:02 +0100

    Add security Configuration for this projetct

--- src/main/kotlin/com/kobe/blogpress_api/BlogpressApiApplication.kt
+++ src/main/kotlin/com/kobe/blogpress_api/BlogpressApiApplication.kt
@@ -3,9 +3,11 @@ package com.kobe.blogpress_api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
 @EnableReactiveMongoRepositories
+@EnableMongoRepositories
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class BlogpressApiApplication
