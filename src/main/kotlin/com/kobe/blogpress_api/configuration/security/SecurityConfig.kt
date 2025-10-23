3a1ae9d93eaecd66950dc186baafd83f37970f2b
Ben Djibril <konedjibril2004@gmail.com>
2025-10-23 03:23:03 +0100

    Add security Configuration for this projetct

--- src/main/kotlin/com/kobe/blogpress_api/configuration/security/SecurityConfig.kt
+++ src/main/kotlin/com/kobe/blogpress_api/configuration/security/SecurityConfig.kt
@@ -32,7 +32,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebSecurity
 @EnableMethodSecurity
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
